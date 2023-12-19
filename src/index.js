ca6259b0fd1431226a0bae730fea32a3a4c3a8c2
Neveod <neve@ravenbay.com>
2023-12-18 19:31:49 -0700

    added basic formhandling and typewriter plugin

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
+function generateGame(event) {
+  event.preventDefault();
+  new Typewriter("#game", {
+    strings: "Hello World",
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
 
+let gameFormElement = document.querySelector("#game-generator-form");
+gameFormElement.addEventListener("submit", generateGame);

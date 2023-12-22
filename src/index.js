eb660085d1aae7b932f2de5e99ac5fe34eb705bd
Neveod <neve@ravenbay.com>
2023-12-21 18:16:35 -0700

    Improved the UX

--- src/index.js
+++ src/index.js
@@ -14,9 +14,13 @@ function generateGame(event) {
   let instructionsInput = document.querySelector("#instructions");
   let apiKey = "b5b6fea0f8dfd4f557b6o7066130170t";
   let context =
-    "You love games and love to find the most interesting games to play offline with not too many instructions. your mission is to generate simple instructions up to 5 lines in basic HTML and a <strong> title and seperate the welcome line from the mission line. Make sure to follow the user instructions below and display them in a list format";
+    "You love games and love to find the most interesting games to play without internet with not too many instructions. your mission is to generate simple instructions up to 5 lines in basic HTML and a <strong></strong> title and seperated with a <div></div> the welcome line from the mission line. Make sure to follow the user instructions below and display them in a <ol></ol> format";
   let prompt = `User instructions: generate a game about ${instructionsInput.value}`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  let gameElement = document.querySelector("#game");
+  gameElement.classList.remove("hidden");
+  gameElement.innerHTML = `<div class="blink">Generating game about ${instructionsInput.value}</div>`;
 
   console.log("Generating Game");
   console.log(`Prompt: ${prompt}`);
